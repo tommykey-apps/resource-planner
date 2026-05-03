4613888ad9f35f125af9cf081fa9c90605a4ed87
tommykey0925 <71512581+tommykey0925@users.noreply.github.com>
2026-05-04 08:32:20 +0900

    feat: Terraform infrastructure for Lambda + CloudFront + DynamoDB
    - Add Terraform configuration (infra/*.tf)
      - Lambda container with Lambda Web Adapter
      - API Gateway HTTP API
      - CloudFront with SSR caching (CachingDisabled + /_app/* cached)
      - DynamoDB Single Table Design (pk/sk)
      - ECR repository with lifecycle policy
      - Route53 + ACM for planner.tommykeyapp.com
      - SSM parameters for CD
    
    - Add web/Dockerfile for Lambda container
    - Change SvelteKit to adapter-node for SSR
    - Add terraform and awscli2 to flox
    
    Closes #4

---
+++ infra/apigateway.tf
@@ -0,0 +1,32 @@
+resource "aws_apigatewayv2_api" "api" {
+  name          = "${var.project}-api"
+  protocol_type = "HTTP"
+
+  tags = {
+    Project = var.project
+  }
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id                 = aws_apigatewayv2_api.api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.app.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.api.id
+  name        = "$default"
+  auto_deploy = true
+
+  tags = {
+    Project = var.project
+  }
+}
---
+++ infra/ecr.tf
@@ -0,0 +1,34 @@
+resource "aws_ecr_repository" "app" {
+  name                 = var.project
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Project = var.project
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = aws_ecr_repository.app.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 5 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 5
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
